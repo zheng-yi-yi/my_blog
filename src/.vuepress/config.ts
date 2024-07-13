ce4386502c72c25199e012cf2b7c2c2a5b76e106
zhengyiyi <zrh182278@outlook.com>
2024-07-13 23:59:25 +0800

    设置base为my_blog，并准备开启GitHub Page

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/my_blog/",
 
   lang: "zh-CN",
   title: "博客演示",
